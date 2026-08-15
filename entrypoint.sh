5dc27bd58c915dbafe7dff51c4e2bc7d7ecb9732
Maksym <mmaxbb@gmail.com>
2026-08-14 21:38:57 -0700

    Fix entrypoint to prevent ffmpeg hangout

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,7 @@ RTSP_URL="${RTSP_URL:-rtsp://192.168.0.24/11}"
 
 while true; do
   echo "Starting FFmpeg stream reader for $RTSP_URL..."
-
-  ffmpeg -loglevel error \
-    -rw_timeout 5000000 \
-    -fflags nobuffer -flags low_delay \
-    -probesize 32 -analyzeduration 0 \
-    -rtsp_transport tcp -i "$RTSP_URL" \
-    -vf "scale=480:320" -q:v 8 -r 15 \
-    -tune zerolatency -preset ultrafast \
-    -update 1 -y /tmp/frame.jpg
-
+  ffmpeg -loglevel error -timeout 5000000 -rtsp_transport tcp -fflags nobuffer -flags low_delay -probesize 32 -analyzeduration 0 -i "$RTSP_URL" -vf "scale=480:320" -q:v 8 -r 15 -tune zerolatency -preset ultrafast -update 1 -y /tmp/frame.jpg
   echo "FFmpeg process died or timed out. Reconnecting in 2 seconds..."
   sleep 2
 done &
